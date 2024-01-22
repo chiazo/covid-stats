be6d837f1a780e599c287fdb3564f37cd6cf8098
chiazo <chiazo@live.unc.edu>
2024-01-21 23:23:13 -0500

    allow notifications

--- covid_stats.py
+++ covid_stats.py
@@ -1,9 +1,27 @@
 from cities.city import City
 import matplotlib.pyplot as plt
-import pandas as pd
 from math import trunc
+import schedule
+import time
+import os
+from dotenv import load_dotenv
+from notify.DeliveryMethod import DeliveryMethod
 
 def main():
+    schedule.every(2).weeks.do(covid_stats)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+
+def covid_stats():
+    load_dotenv()
+
+    PHONE_NUMBER = os.getenv('PHONE_NUMBER')    
+    EMAIL = os.getenv('EMAIL')    
+    CARRIER = os.getenv('CARRIER') 
+
     df = City("new_york").get_df().sort_values(["sample_date", "site_name"])
     
     site_date_df = df.groupby([df["sample_date"].dt.date, df["site_name"]])[["copies_avg_flowrate"]].mean()
@@ -18,16 +36,27 @@ def main():
 
     pct_change_from_high = trunc(((curr[1] - peak[1]) / curr[1]) * 100)
     pct_change_from_low = trunc(((curr[1] - low[1]) / curr[1]) * 100)
+    message = '''
+    Peak: {0} - avg of {1} copies/ml.
+    Low:  {2} - avg of {3} copies/ml.
+    --------------------------------------------------------------
+    As of {4}, there's been a {5}% decrease since {6} with an avg reading of {7} copies/ml. This is a {8}% increase since the low on {9}.
+    '''.format(peak[0], shorten_number(peak[1]), low[0], shorten_number(low[1]), curr[0], pct_change_from_high, peak[0], shorten_number(curr[1]), pct_change_from_low, low[0])
+    
+    DeliveryMethod(EMAIL, PHONE_NUMBER, CARRIER, message)
+
+    
     print(f"Peak: {peak[0]} - avg of {shorten_number(peak[1])} copies/ml.")
     print(f"Low:  {low[0]} - avg of {shorten_number(low[1])} copies/ml.")
     print("--------------------------------------------------------------")
     print(f"As of {curr[0]}, there's been a {pct_change_from_high}% decrease since {peak[0]} with an avg reading of {shorten_number(curr[1])} copies/ml.")
     print(f"This is a {pct_change_from_low}% increase since the low on {low[0]}.")
 
+    print(message)
     # print results to local csv
     site_date_df.to_csv("site_date.csv", sep='\t', encoding='utf-8')
     site_df.to_csv("site.csv", sep='\t', encoding='utf-8')
-    date_df.to_csv("date.csv", sep='\t', encoding='utf-8')
+    date_df.to_csv("date.csv", sep='\t', encoding='utf-8')     
 
 def shorten_number(num):
     num = trunc(num)
---
+++ notify/DeliveryMethod.py
@@ -0,0 +1,43 @@
+import os
+from os import environ as env
+from dotenv import load_dotenv
+from datetime import date
+from .PyMessenger import Email, Messenger
+from etext import send_sms_via_email
+
+
+class DeliveryMethod:
+    carriers = {
+        "att": "@txt.att.net",
+        "tmobile": "@tmomail.net",
+        "verizon": "@vtext.com",
+        "sprint": "@messaging.sprintpcs.com"
+    }
+
+    def __init__(self, email, phone, carrier, message) -> None:
+        self.email = email
+        self.phone = phone
+        self.carrier = carrier
+        self.message = message
+        self.send()
+
+    def send(self):
+        load_dotenv(
+            f"{os.path.dirname(os.path.dirname(os.path.abspath(__file__)))}/.env")
+        today = date.today().strftime("%m/%d/%Y")
+        subject = f'[covid-stats] - {today}'
+        # self.send_text(self.message, subject)
+        self.send_email(self.message, subject)
+
+    def send_text(self, content, subject):
+        sender_credentials = (env['GMAIL_USER'], env['GMAIL_PASSWORD'])
+
+        return send_sms_via_email(
+            self.phone, content, self.carrier, sender_credentials, subject=subject
+        )
+
+    def send_email(self, content, subject):
+        my_messenger = Messenger(env['GMAIL_USER'], env['GMAIL_PASSWORD'])
+        to = self.email
+        msg = Email(to, subject, content, is_HTML=False)
+        return my_messenger.send_email(msg, one_time=True)
